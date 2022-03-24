bf7e8897a57f94d642ea5bae548fa6812a4d67b5
Donovan Gallaway <donovangallaway@gmail.com>
2022-03-23 23:55:29 -0700

    added basic nav and styling

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,16 @@
 <script>
-	export let name;
+	import Nav from "./components/Nav.svelte";
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
-
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
+	main{
+		background-image: linear-gradient(rgb(207,207,207), white);
+		height: 100%;
+		margin-top: 4.5em;
 	}
 </style>
+
+<Nav/>
+<main>
+<h1>Hello World</h1>
+</main>
---
+++ src/components/Nav.svelte
@@ -0,0 +1,36 @@
+<style>
+    nav{
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        color: white;
+        background-color: rgb(17,37,89);
+        position: fixed;
+        top: 0;
+        height: 4.5em;
+    }
+
+    nav > *{
+        margin-right: 2em;
+    }
+
+    .logo{
+        margin-left: 1.5em;
+    }
+
+    .link{
+        display: flex;
+        flex-direction: row;
+        justify-content: space-around;
+        width: 80vw;
+    }
+</style>
+
+<nav>
+    <div class="logo"><h1>JJ&E Web Design</h1></div>
+    <div class="link">
+        <h3>Portfolio</h3>
+        <h3>About</h3>
+        <h3>Contact Us</h3>
+    </div>
+</nav>
